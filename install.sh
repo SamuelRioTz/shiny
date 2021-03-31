7bd779f97585fe83313d986e7a537f5f472dd6f8
Samuel Rioja <samuelriotz@hotmail.com>
2021-03-31 18:03:23 -0400

    init commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo su - -c "R -e \"install.packages('shiny')\"" &&
+sudo su - -c "R -e \"install.packages('datasets')\"" &&
+sudo su - -c "R -e \"install.packages('dplyr')\"" &&
+sudo su - -c "R -e \"install.packages('tidyr')\"" &&
+sudo su - -c "R -e \"install.packages('ggplot2')\"" &&
+sudo su - -c "R -e \"install.packages('rsconnect')\"" &&
+sudo su - -c "R -e \"install.packages('shinydashboard')\"" &&
+sudo su - -c "R -e \"install.packages('sjlabelled')\"" &&
+sudo su - -c "R -e \"install.packages('httr')\"" &&
+sudo su - -c "R -e \"install.packages('haven')\"" &&
+sudo su - -c "R -e \"install.packages('rworldmap')\"" &&
+sudo su - -c "R -e \"install.packages('leaflet')\"" &&
+sudo su - -c "R -e \"install.packages('pps')\""
